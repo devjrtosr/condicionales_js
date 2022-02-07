11e4e1ebe95f046ad979d1105fd8d399d88a2507
Jlanza12 <lanza3524@gmail.com>
2022-02-07 14:02:03 -0600

    agregando tarea de condicionales

---
+++ app.Aaron.js
@@ -0,0 +1,12 @@
+    let cadenaDeTexto = prompt("Digite una cadena de texto");
+    console.log(cadenaDeTexto); 
+}
+
+if (cadenaDeTexto.length > 15) {
+    console.log("hola"); 
+} else {
+    console.log("Mundo");
+} 
+ 
+
+
